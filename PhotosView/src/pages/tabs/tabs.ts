fd28ef144e36a444ca667ad56590d23901c4e674
schubertq <878393556@qq.com>
2017-01-10 16:27:38 +0800

    初始化项目

---
+++ PhotosView/src/pages/tabs/tabs.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+import { HomePage } from '../home/home';
+
+@Component({
+  templateUrl: 'tabs.html'
+})
+export class TabsPage {
+  tab1Root: any = HomePage;
+
+  constructor() {
+
+  }
+}
